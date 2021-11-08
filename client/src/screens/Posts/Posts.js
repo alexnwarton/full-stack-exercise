9b451f6ad851aa84853eeb232523c65b07ddec61
Lance Walker <LWalk2678@msn.com>
2021-11-08 16:22:31 -0700

    added code to services/posts

--- client/src/screens/Posts/Posts.js
+++ client/src/screens/Posts/Posts.js
@@ -1,8 +1,10 @@
 import Layout from '../../components/Layout/Layout.jsx'
 import { useState, useEffect } from 'react'
 import { getPosts } from '../../services/posts.js'
+
 const Posts = () => {
   const [posts, setPosts] = useState([])
+
   useEffect(() => {
     const fetchPosts = async () => {
       const allPosts = await getPosts()
@@ -11,13 +13,30 @@ const Posts = () => {
       console.log(allPosts)
     }
     fetchPosts();
-  },[])
+  }, [])
+  
+
   return (
     <Layout>
-		<div>
-			<h1>Test Posts.js</h1>
+		  <div>
+			  <h1>Test Posts.js</h1>
       </div>
-      </Layout>
+      {/* <Search onSubmit={handleSubmit} handleSearch={handleSearch} />
+      <Sort onSubmit={handleSubmit} handleSort={handleSort} />
+      <div className='posts'>
+        {searchResult.map((post, index) => {
+          return (
+            <Product
+              _id={post._id}
+              title={post.title}
+              imgURL={post.imgURL}
+              publisher={post.publisher}
+              key={index}
+            />
+          )
+        })}
+      </div> */}
+    </Layout>
 	)
 }
 
