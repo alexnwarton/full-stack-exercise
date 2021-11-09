cfef34f2c83d9780416496e0e13163946e5d40ef
elhadj soumahoro <ejsoum@elhadjs-MBP.home>
2021-11-09 16:10:42 -0500

    createPost fully functioning

--- client/src/screens/PostCreate/PostCreate.js
+++ client/src/screens/PostCreate/PostCreate.js
@@ -1,8 +1,75 @@
 import Layout from '../../components/Layout/Layout.jsx'
+import { createPost } from '../../services/posts.js'
+import { useEffect, useState } from 'react'
 const PostCreate = () => {
+  const [newPost, setNewPost] = useState({
+    title: '',
+    review: '',
+    imgURL: '',
+    publisher: ''
+})
+const handleChange = (event) => {
+  const { name, value  } = event.target
+  setNewPost({
+          ...newPost,
+          [name]: value,
+          
+  })
+}
+  
+  const handleSubmit = async (event) => {
+    event.preventDefault()
+    const created = await createPost(newPost)
+    console.log('button clicked!')
+    alert('Your Blog has been  created!')
+    setNewPost({
+      title: '',
+      review: '',
+      imgURL: '',
+      publisher: ''
+  })
+  }
+  // useEffect(() => {
+  //   const createPost = async () => {
+  //     const createdPost = await createPost()
+      
+  //     console.log(createdPost)
+      
+  //   }
+  //   createPost();
+  // }, [])
   return (
     <Layout>
-		<div>
+      <div>
+        <form onSubmit={handleSubmit}>
+        <input placeholder="Title"
+          value={newPost.title}
+            name="title"
+            onChange={handleChange}
+          />
+        <input placeholder="Img Link"
+          value={newPost.imgURL}
+            name="imgURL"
+            onChange={handleChange}
+            
+          />
+          
+        <textarea 
+          className="textarea-post-review"
+          placeholder="Review"
+          value={newPost.review}
+          name="review"
+          onChange={handleChange}
+          />
+          <input 
+          className="input-post-publisher"
+          placeholder="Publisher"
+          value={newPost.publisher}
+          name="publisher"
+          onChange={handleChange}
+          />
+          <button type="submit" className="update-button">Create Post</button>
+        </form>
 			<h1>Test PostCreate.js</h1>
       </div>
       </Layout>
