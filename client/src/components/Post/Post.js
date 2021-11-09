680a54d194840bb8a2a540b9ed13220f03dce7ec
Lance Simmons <lancehowardsimmons@gmail.com>
2021-11-09 15:41:25 -0500

    Post page is linking to details page for each post

--- client/src/components/Post/Post.js
+++ client/src/components/Post/Post.js
@@ -1,10 +1,16 @@
+import { Link } from 'react-router-dom'
+
+
 const Post = ({ _id, title, imgURL, review, publisher, key }) => {
   return (
-    <div>
-      <h2>{title}</h2>
-      <p>{review}</p>
-      <h3>{publisher}</h3>
-    </div>
+    <Link to = {`/posts/${_id}`}>
+      <div>
+        <h2>{title}</h2>
+        <img src={imgURL}/>
+        <p>{review}</p>
+        <h3>{publisher}</h3>
+      </div>
+    </Link>
   )
 }
 
